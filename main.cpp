549cf82cac2ae9c1178b3b1e575da28de3aea020
Victor3699 <131671439+Victor3699@users.noreply.github.com>
2023-05-17 10:59:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main()
     drawer.drawRectangle(100, 100, 200, 150, 2, cv::Scalar(0, 255, 0));
     drawer.drawRectangle(400, 300, 100, 100, -1, cv::Scalar(0, 0, 255));
     drawer.showImage();
-
+    circle = Circle(20);
+    circle.draw();
     return 0;
 }
