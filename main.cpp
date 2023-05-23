964e285fbf15eae7955da3ec265cd1fb011265ab
filsuin <131671065+filsuin@users.noreply.github.com>
2023-05-23 17:53:12 +0200

    Update main.cpp
    Ajout de l'inclusion des classes:
    -CircleDrawer.h
    -LineDrawer.h

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
-#include "cercle.h"
-#include "rectangle.h"
-#include "shape.h"
+#include "CircleDrawer.h"
+#include "RectangleDrawer.h"
+#include "Shape.h"
+#include "TriangleDrawer.h"
+#include "LineDrawer.h"
 
 int main() {
     const std::string windowName = "Drawing Window";
@@ -11,10 +13,14 @@ int main() {
     // Création des formes
     Shape* rectangle = new RectangleDrawer(windowName, 200, 150, 100, 100, 2, 255, 0, 0);
     Shape* circle = new CircleDrawer(windowName, 75, 400, 300, 3, 0, 0, 255);
+    Shape* triangle = new TriangleDrawer(windowName, 200, 100, 300, 250, 400, 100, 2, 0, 255, 0);
+    Shape* line = new LineDrawer(windowName, 100, 400, 600, 400, 2, 255, 255, 0);
 
     // Dessiner les formes sur l'image
     rectangle->draw(image);
     circle->draw(image);
+    triangle->draw(image);
+    line->draw(image);
 
     // Afficher l'image dans la fenêtre
     cv::imshow(windowName, image);
@@ -24,6 +30,8 @@ int main() {
     cv::destroyWindow(windowName);
     delete rectangle;
     delete circle;
+    delete triangle;
+    delete line;
 
     return 0;
 }
