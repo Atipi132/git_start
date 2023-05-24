077d3ecf98edbf1fc2640d67cdaead8d16de7481
filsuin <131671065+filsuin@users.noreply.github.com>
2023-05-24 23:29:53 +0200

    Update shape.h

--- shape.h
+++ shape.h
@@ -1,15 +1,15 @@
 #pragma once
 #include <opencv2/opencv.hpp>
+#include "Drawable.h"
 
-class Shape {
+class Shape : public Drawable {
 public:
     Shape(const std::string& windowName, int thickness, int r, int g, int b)
         : windowName_(windowName), thickness_(thickness), color_(cv::Scalar(b, g, r)) {
         cv::namedWindow(windowName_);
     }
 
-    virtual void draw(cv::Mat& image) = 0;
-    virtual void update() = 0;
+    virtual ~Shape() {}
 
 protected:
     std::string windowName_;
