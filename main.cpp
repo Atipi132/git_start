0d659ef923e16e8a53c915c579dcfc09ecd9b2c7
Victor3699 <131671439+Victor3699@users.noreply.github.com>
2023-05-25 16:11:37 +0200

    Update main.cpp
    Rajout des commentaires

--- main.cpp
+++ main.cpp
@@ -10,27 +10,33 @@ int main() {
     const int imageHeight = 600;
     cv::Mat image(imageHeight, imageWidth, CV_8UC3, cv::Scalar(255, 255, 255));
 
-    std::vector<Drawable*> shapes;
+    std::vector<Drawable*> shapes;// Crée un vecteur pour stocker les formes dessinables
+    
+    // Ajoute différentes formes au vecteur
     shapes.push_back(new TriangleDrawer(windowName, 200, 100, 300, 250, 400, 100, 2, 0, 255, 0));
     shapes.push_back(new RectangleDrawer(windowName, 200, 150, 100, 100, 2, 255, 0, 0));
     shapes.push_back(new CircleDrawer(windowName, 75, 400, 300, 3, 0, 0, 255));
     shapes.push_back(new LineDrawer(windowName, 100, 400, 600, 400, 2, 255, 255, 0));
 
     bool isAnimating = true;
-
+    
+    // Boucle principale de l'animation
     while (isAnimating) {
         for (Drawable* shape : shapes) {
             shape->draw(image);
             shape->update();
         }
-
+        
+        // Affiche l'image dans une fenêtre OpenCV
         cv::imshow(windowName, image);
-
+        
+         // Vérifie si une touche est enfoncée pour arrêter l'animation
         if (cv::waitKey(10) != -1) {
             isAnimating = false;
         }
     }
-
+    
+    // Détruit la fenêtre OpenCV et libère la mémoire des formes
     cv::destroyWindow(windowName);
     for (Drawable* shape : shapes) {
         delete shape;
