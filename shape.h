356f8db958e380c240455b87b5dd0474dc5f0b09
Atipi132 <hugo.pothieux@gmail.com>
2023-05-25 17:03:29 +0200

    Update shape.h
    Got rid of useless method instructions that were already added to shape.cpp

--- shape.h
+++ shape.h
@@ -4,10 +4,7 @@
 
 class Shape {
 public:
-    Shape(const std::string& windowName, int thickness, int r, int g, int b)
-        : windowName_(windowName), thickness_(thickness), color_(cv::Scalar(b, g, r)) {
-        cv::namedWindow(windowName_);
-    }
+    Shape(const std::string& windowName, int thickness, int r, int g, int b);
 
     virtual ~Shape() {} // Ajout d'un destructeur virtuel
 
