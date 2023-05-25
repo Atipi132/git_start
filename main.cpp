19938d833dbbd6935c6e1a76e7972937be8d9fa8
REMONNAY Max <131671065+filsuin@users.noreply.github.com>
2023-05-25 14:10:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,33 @@
+#include <iostream>
 #include <opencv2/opencv.hpp>
-#include "rectangle.h"
-#include "Cercle.h"
-int main()
-{
-    RectangleDrawer drawer("Rectangles", 800, 600);
-    drawer.drawRectangle(100, 100, 200, 150, 2, cv::Scalar(0, 255, 0));
-    drawer.drawRectangle(400, 300, 100, 100, -1, cv::Scalar(0, 0, 255));
-    drawer.showImage();
-    circle = Circle(20);
-    circle.draw();
-    return 0;
+#include "shape.h"
+#include "RectangleDrawer.h"
+#include "CircleDrawer.h"
+
+int main() {
+    const std::string windowName = "Drawing Window";
+    const int imageWidth = 800;
+    const int imageHeight = 600;
+    cv::Mat image(imageHeight, imageWidth, CV_8UC3, cv::Scalar(255, 255, 255));
+
+    // Création des formes
+    Shape* rectangle = new RectangleDrawer(windowName, 200, 150, 100, 100, 2, 255, 0, 0);
+    Shape* circle = new CircleDrawer(windowName, 75, 400, 300, 3, 0, 0, 255);
+
+    // Dessiner les formes sur l'image
+    rectangle->draw(image);
+    circle->draw(image);
+
+    // Afficher l'image dans la fenêtre
+    cv::namedWindow(windowName);
+    cv::resizeWindow(windowName, imageWidth, imageHeight); // Redimensionner la fenêtre à la taille de l'image
+    cv::imshow(windowName, image);
+    cv::waitKey(0);
+
+    // Nettoyage
+    cv::destroyWindow(windowName);
+    delete rectangle;
+    delete circle;
+
+	return 0;
 }
