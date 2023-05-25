3e7ccdb2575d748d8d909b6549ebf6858acb7b96
Victor3699 <131671439+Victor3699@users.noreply.github.com>
2023-05-25 17:20:56 +0200

    Delete RectangleDrawer.cpp

--- RectangleDrawer.cpp
+++
@@ -1,9 +0,0 @@
-#include "RectangleDrawer.h"
-
-RectangleDrawer::RectangleDrawer(const std::string& windowName, int width, int height, int x, int y,
-    int thickness, int r, int g, int b)
-    : Shape(windowName, thickness, r, g, b), width_(width), height_(height), x_(x), y_(y) {}
-
-void RectangleDrawer::draw(cv::Mat& image) {
-    cv::rectangle(image, cv::Point(x_, y_), cv::Point(x_ + width_, y_ + height_), color_, thickness_);
-}
