4f66e4f049ccdd6a33a66961a230492fca809bfc
Atipi132 <hugo.pothieux@gmail.com>
2023-06-17 18:07:43 +0200

    Update RectangleDrawer.cpp
    Created 'update' function to animate rectangle

--- RectangleDrawer.cpp
+++ RectangleDrawer.cpp
@@ -7,3 +7,10 @@ RectangleDrawer::RectangleDrawer(const std::string& windowName, int width, int h
 void RectangleDrawer::draw(cv::Mat& image) {
     cv::rectangle(image, cv::Point(x_, y_), cv::Point(x_ + width_, y_ + height_), color_, thickness_);
 }
+
+void RectangleDrawer::update(int x, int y, cv::Mat& image) {     // Mettre à jour les coordonnées du cercle en fonction des paramètres d'animation
+    cv::rectangle(image, cv::Point(x_, y_), cv::Point(x_ + width_, y_ + height_), cv::Scalar(255, 255, 255), thickness_);
+    x_ += x;
+    y_ += y;
+    draw(image);
+}
