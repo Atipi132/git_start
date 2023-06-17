b2cddcef348d3cad0f4434b2e7ad603f58ba882b
Atipi132 <hugo.pothieux@gmail.com>
2023-06-17 18:08:50 +0200

    Update main.cpp
    updated main to show how to use the 'update' function of the circle and the rectangle

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "shape.h"
 #include "RectangleDrawer.h"
 #include "CircleDrawer.h"
+#include <thread>
+#include <chrono>
 
 int main() {
     const std::string windowName = "Drawing Window";
@@ -22,7 +24,12 @@ int main() {
     cv::namedWindow(windowName);
     cv::resizeWindow(windowName, imageWidth, imageHeight); // Redimensionner la fenêtre à la taille de l'image
     cv::imshow(windowName, image);
-    cv::waitKey(0);
+    cv::waitKey(1);
+    std::this_thread::sleep_for(std::chrono::seconds(2)); //attendre 2 secondes avant de changer le position du cercle et du rectangle
+    circle->update(100, 100, image);
+    rectangle->update(-50, 50, image);
+    cv::imshow(windowName, image);
+    cv::waitKey(0); // attendre que l'utilisateur appuie sur une touche pour sortir detruire la fenetre
 
     // Nettoyage
     cv::destroyWindow(windowName);
