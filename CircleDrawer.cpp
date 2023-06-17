b88bb727885062280646b4bcb5f036baaa050df9
Atipi132 <hugo.pothieux@gmail.com>
2023-06-17 17:31:35 +0200

    Update CircleDrawer.cpp
    Updated CircleDrawer.cpp to fix bug

--- CircleDrawer.cpp
+++ CircleDrawer.cpp
@@ -1,25 +1,16 @@
-#pragma once
-#include <opencv2/opencv.hpp>
-#include "shape.h"
+#include "CircleDrawer.h"
 
-class CircleDrawer : public Shape {
-public:
-    CircleDrawer(const std::string& windowName, int radius, int centerX, int centerY, int thickness, int r, int g, int b)
-        : Shape(windowName, thickness, r, g, b), radius_(radius), centerX_(centerX), centerY_(centerY) {}
+CircleDrawer::CircleDrawer(const std::string& windowName, int radius, int centerX, int centerY, int thickness, int r, int g, int b)
+    : Shape(windowName, thickness, r, g, b), radius_(radius), centerX_(centerX), centerY_(centerY)
+{
+}
 
-    void draw(cv::Mat& image) override {
-        cv::circle(image, cv::Point(centerX_, centerY_), radius_, color_, thickness_);
-    }
+void CircleDrawer::draw(cv::Mat& image)
+{
+    cv::circle(image, cv::Point(centerX_, centerY_), radius_, color_, thickness_);
+}
 
-    void update(){     // Mettre à jour les coordonnées du cercle en fonction des paramètres d'animation
-        centerX_ += speedX_;
-        centerY_ += speedY_;
-    }
-
-private:
-    int radius_;
-    int centerX_;
-    int centerY_;
-    int speedX_ = 2;
-    int speedY_ = 1;
-};
+void CircleDrawer::update(){     // Mettre à jour les coordonnées du cercle en fonction des paramètres d'animation
+    centerX_ += 2;
+    centerY_ += 1;
+}
