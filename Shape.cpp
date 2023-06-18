7ca379b90a01131394157ef497bf63184bdd6787
REMONNAY Max <131671065+filsuin@users.noreply.github.com>
2023-06-18 15:06:32 +0200

    Delete Shape.cpp

--- Shape.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "Shape.hpp"
-
-Shape::Shape(const std::string& windowName, int thickness, int r, int g, int b)
-    : windowName_(windowName), thickness_(thickness), color_(cv::Scalar(b, g, r)) {
-    cv::namedWindow(windowName_);
-}
-
-Shape::~Shape() {}
